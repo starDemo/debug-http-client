5ffc9a3daf675b922382daf829fb32b9aaf354a1
starliu <starliu@thecover.co>
2019-10-18 16:43:48 +0800

    [add] 日志写入tmp，停止stdout输出

--- boot/boot.go
+++ boot/boot.go
@@ -1,8 +1,13 @@
 package boot
 
-import "github.com/gogf/gf/frame/g"
+import (
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/os/glog"
+)
 
 func init() {
+	glog.SetPath("/tmp/debug-client")
+	glog.SetStdoutPrint(false)
     g.Server().SetPort(8199)
     g.Server().SetDumpRouteMap(false)
     g.Server().SetAccessLogEnabled(true)
