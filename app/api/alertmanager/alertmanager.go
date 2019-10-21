85fb9e714825d89c8fd6260cc6af7418848320eb
starliu <starliu@thecover.co>
2019-10-21 11:36:28 +0800

    [fix] 修改为日志输出

--- app/api/alertmanager/alertmanager.go
+++ app/api/alertmanager/alertmanager.go
@@ -3,16 +3,15 @@ package alertmanager
 import (
 	"debug-http-client/library/response"
 	"debug-http-client/library/types"
-	"fmt"
 	"github.com/gogf/gf/encoding/gjson"
 	"github.com/gogf/gf/net/ghttp"
 	"github.com/gogf/gf/os/glog"
 )
 var alertmanager types.Alertmanager
 func Alert(r *ghttp.Request){
-	if gjson.DecodeTo(r.GetRaw(), &alertmanager) != nil {
-		glog.Error("返回JSON 反序列化失败 ")
+	if err:=gjson.DecodeTo(r.GetRaw(), &alertmanager); err != nil {
+		glog.Error("返回JSON 反序列化失败 ",err)
 	}
-	fmt.Println(alertmanager.Status,":",alertmanager.Alerts[0].Labels.AlertName)
+	glog.Info(alertmanager.Status,":",alertmanager.Alerts[0].Labels.AlertName)
 	response.Json(r, 0, "ok")
 }
